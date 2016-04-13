366c5eae85271e45a0b3b868cdddc23e20c659b9
Abhinav Jha <abhinav.043@gmail.com>
2016-04-13 17:35:42 -0400

    Adding word count

---
+++ samples/problem3/src/gosample/gosample.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"os"
+	"regexp"
+)
+
+func readURL(url string) string {
+	// Read the URL and return body contents
+
+	resp, err := http.Get(url)
+	if err != nil {
+		// URL Error 
+		os.Exit(1)
+	}	
+	
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// Read error
+		os.Exit(1)
+	}	
+
+	return string(body)
+}
+
+func get_words (contents string) []string{
+	// make a list of all distinct words
+
+	words := regexp.MustCompile("\\w+")
+	return words.FindAllString(contents, -1)
+}
+
+func count_words (words []string) map[string]int {
+	// Count the frequency
+
+	word_counts := make(map[string]int)
+	for _, word := range words {
+		word_counts[word]++
+	}	
+	return word_counts
+}
+
+func write_to_file (word_counts map[string]int) {
+	for word, word_count := range word_counts {
+		fmt.Printf("%v %v\n", word, word_count)
+	}
+}
+
+func main() {
+	url := "https://raw.githubusercontent.com/aporeto-inc/internship2016/master/samples/problem2/uniquified_file.txt"
+
+	content := readURL(url)
+	write_to_file(count_words(get_words(content)))	
+}
