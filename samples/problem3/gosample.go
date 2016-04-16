4ea31ad9aa0b08ec136a3720c3331cc69e5d5fa5
Abhinav Jha <abhinav.043@gmail.com>
2016-04-16 17:53:54 -0400

    Serial Execution commit

--- samples/problem3/gosample.go
+++ samples/problem3/gosample.go
@@ -18,7 +18,8 @@ func readURL(url string) string {
 	resp, err := http.Get(url)
 	if err != nil {
 		// URL Error 
-        panic(err)
+		fmt.Printf("URL error: \"%s\", Exiting...\n", url)
+        //panic(err)
         os.Exit(1)
 	}	
 	
@@ -26,7 +27,7 @@ func readURL(url string) string {
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		// Read error
-        panic(err)
+		fmt.Printf("URL Content Read Error: \"%s\". Exiting...\n", url);
 		os.Exit(1)
 	}	
 
@@ -51,10 +52,12 @@ func count_words (words []string) map[string]int {
 }
 
 func write_to_file (word_counts map[string]int, filename string, url string) {
+	// Write frequency table to output file
 
 	f, err := os.Create(filename)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Can't create file: \"%s\", Exiting...\n", filename)
+		os.Exit(1)
 	}
 
 	defer f.Close()
@@ -67,15 +70,16 @@ func write_to_file (word_counts map[string]int, filename string, url string) {
 
     n, err := f.WriteString(write_content)
     if err != nil {
-        panic(err)
+		fmt.Printf("Unable to write to file: \"%s\", Exiting...\n", filename)
+		os.Exit(1)
     }
 
-    _ = n
+    _ = n // Not using
 
 }
 
 func (urls *stringslice) String() string {
-    return fmt.Sprintf("%s", *urls)
+    return fmt.Sprintf("url1,url2,...\t")
 }
 
 func (urls *stringslice) Set(value string) error {
@@ -88,13 +92,19 @@ func (urls *stringslice) Set(value string) error {
 func main() {
 
     //default_url := "https://raw.githubusercontent.com/aporeto-inc/internship2016/master/samples/problem2/uniquified_file.txt"
+	flag.Usage = func() {
+		// Modifying usage function for better readability
+		fmt.Printf("Usage: %s -urls=<comma-separated-one-or-more-urls>\n\n", os.Args[0])
+		flag.PrintDefaults()
+	}
 
     var urls stringslice
-    flag.Var(&urls, "urls", "where value is comma-separated list of urls")
+    flag.Var(&urls, "urls", "Comma-separated list of urls")
     flag.Parse()
 
     if flag.NFlag() == 0 {
-        flag.PrintDefaults()
+		fmt.Printf("Invalid command: No arguments provided\n")
+		flag.Usage()
         os.Exit(1)
     }
 
